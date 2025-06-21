dd7cb95a18fd3d7a586b0b85cc221ecdf5bc4e91
Sedana Yoga <9g.yoga27@gmail.com>
2025-06-21 16:21:19 +0800

    fix: try to use basePath

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: process.env.CI ? "/game-lan" : "",
 };
 
 export default nextConfig;
